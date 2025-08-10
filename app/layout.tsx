6674e443ee875e2499957e96bed43aac7b125bf9
Rokeeb Maruf <dayorokeeb234@gmail.com>
2025-08-10 17:31:32 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body>
+      <body className="max-w-full">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
           {children}
         </ThemeProvider>

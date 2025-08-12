20a273edc57e31d6dd26207503d04c53cfc50e0c
Rokeeb Maruf <dayorokeeb234@gmail.com>
2025-08-13 00:39:17 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -42,17 +42,17 @@ export default function Home() {
       }
   }
   return (
-    <div className="bg-gray-100 min-h-screen dark:bg-black text-gray-800 dark:text-white max-w-screen">
+    <div className="bg-gray-100 min-h-screen dark:bg-gray-800 text-gray-800 dark:text-white max-w-screen">
       <header className="bg-white dark:bg-gray-700 top-0 left-0 w-full flex justify-between px-[50px] max-sm:px-[20px] sticky py-[25px] border-b-[3px] border-gray-300 ">
         <p className="font-bold dark:text-white">Where in the world?</p>
         <Toggle/>
       </header>
-      <main className="bg-gray-100 dark:bg-gray-600">
+      <main className="bg-gray-100 dark:bg-gray-700">
         <div className="search items-center flex sticky bg-gray-100 justify-between  left-0 dark:bg-gray-800 px-[clamp(20px,2.6vw,50px)]  sm:px-[20px] max-w-screen py-[25px] top-[75px]">
           <input type="search" placeholder="Search for a country..." id="" onChange={handleChange} className="dark:text-white dark:placeholder-white dark:border-white dark:bg-gray-700 h-9.5 text-sm outline-0 pl-7.5 max-w-lg max-md:min-w-[350px] max-sm:min-w-[200px] rounded relative leading-none shadow-[0_0_10px_rgba(0,0,0,0.4)] bg-white" />
           <img src='./icons8_search.svg' onClick={()=>handleChange} className="absolute w-[24px] rounded cursor-pointer left-[clamp(20px,2.6vw,50px)]"/>
           <div>
-            <select name="countries" id="" className="text-lg dark:bg-gray-800 max-md:text-md max-sm:text-sm">
+            <select name="countries" id="" className="text-lg dark:bg-gray-700 max-md:text-md max-sm:text-sm">
               <option onClick={()=>setCount('none')}>Filter by Region</option>
               <option onClick={()=>setCount('Africa')} value="Africa">Africa</option>
               <option onClick={()=>setCount('America')} value="America">America</option>

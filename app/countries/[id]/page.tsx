485e44d957a533bfb71d2778f2a70f8b64e7d821
Rokeeb Maruf <dayorokeeb234@gmail.com>
2025-08-12 15:44:50 +0100

    Update page.tsx

--- app/countries/[id]/page.tsx
+++ app/countries/[id]/page.tsx
@@ -46,7 +46,7 @@ export default function Country(){
     }, [id])
 
     return(
-        <div className="bg-gray-100 min-h-screen dark:bg-gray-800 text-gray-950 dark:text-white">
+        <div className="bg-gray-100 min-h-screen dark:bg-grey text-gray-950 dark:text-white">
             <header className="bg-white dark:bg-gray-700 top-0 left-0 w-full flex justify-between px-[50px] max-sm:px-[20px] sticky py-[25px] border-b-[3px] border-gray-300">
                 <p className="font-bold">Where in the world?</p>
                 <Toggle/>
